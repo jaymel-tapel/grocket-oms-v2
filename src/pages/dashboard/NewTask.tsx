c5923ef6ba6761be718e7581a4327fb7b5b031c2
Ariel Agdon <arielagdon@yahoo.com>
2023-12-26 19:47:36 +0800

    added task form

--- src/pages/dashboard/NewTask.tsx
+++ src/pages/dashboard/NewTask.tsx
@@ -3,8 +3,15 @@ import TaskForm from "../../components/dashboard/tasks/TaskForm";
 const NewTask = () => {
   return (
     <div>
-      New task / Update task page component. This is the component where you'll
-      add '/Dashboard/Add Task'
+      <div className="flex mt-4 mb-6">
+        <div>
+          <span className="flex gap-2">
+            <p className="text-black text-base">Dashboard</p> /{" "}
+            <p className="text-black text-base">My Task</p> /
+            <p className="text-[#41B2E9] text-base">Add Task</p>
+          </span>
+        </div>
+      </div>
       <TaskForm />
     </div>
   );
