45a78ffc37cb44c7832dc96430bfd0282d4f5799
Ariel Agdon <arielagdon@yahoo.com>
2024-01-11 20:56:59 +0800

    Login page with API

---
+++ src/services/queries/userQueries.ts
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { setAuthorization } from "../../utils/utils";
+
+type loginDetails = {
+  email: string;
+  password: string;
+};
+
+const API_URL = import.meta.env.VITE_API_URL;
+const loginUrl = API_URL + "/auth/login";
+
+export const useLogin = () => {
+  return useMutation({
+    mutationFn: (payload: loginDetails) => {
+      return axios.post(loginUrl, payload);
+    },
+    onSuccess: ({ data }) => {
+      setAuthorization(data.access_token);
+    },
+  });
+};
