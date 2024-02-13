03160e3c6c14865763b6d54b1434febfe9089520
Ariel Agdon <arielagdon@yahoo.com>
2024-02-13 22:00:28 +0800

    dashboard updates

--- src/components/dashboard/tasks/DashboardTasks.tsx
+++ src/components/dashboard/tasks/DashboardTasks.tsx
@@ -64,7 +64,7 @@ const DashboardTasks: React.FC = () => {
 
       timeout = setTimeout(() => {
         clearInterval(fetchInterval);
-      }, 6000);
+      }, 2000);
     };
 
     startFetching();
